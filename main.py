dadd19e093ee3f41c35372ba7e48f72b77738ee8
Jon Alc <jon.f.alcantara@gmail.com>
2025-02-23 18:18:26 -0800

    Can now access channel and get messages

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import discord
-from tokens import BOT_TOKEN
+from tokens import BOT_TOKEN, DAYS_OF_DEV_CHANNEL_ID
 
 class MyClient(discord.Client):
+
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
+        self.dev_log_channel = self.get_channel(DAYS_OF_DEV_CHANNEL_ID)
+        print(f"Accessed {self.dev_log_channel}, getting posts...")
+        
+        messages = [message async for message in self.dev_log_channel.history(limit=10)]
+        for message in messages:
+            print(message)
 
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
+    # async def on_message(self, message):
+    #     print(f"Message from {message.author}: {message.content}")
     
 intents = discord.Intents.default()
 intents.message_content = True
